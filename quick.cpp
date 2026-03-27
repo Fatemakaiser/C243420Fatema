b457aa9ce78a733b6e0a0f0443636256eba0530f
Fatemakaiser <175226822+Fatemakaiser@users.noreply.github.com>
2026-03-27 20:15:33 +0600

    Add files via upload

---
+++ quick.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void quickSort(int arr[],int left,int right) {
+    int i=left,j=right;
+    int pivot=arr[(left+right)/2];
+
+    while (i<=j) {
+        while (arr[i] < pivot) i++;
+        while (arr[j] > pivot) j--;
+
+        if (i<=j) {
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+    }
+
+    if (left<j)
+        quickSort(arr,left,j);
+    if (i<right)
+        quickSort(arr,i,right);
+}
+int main() {
+    int n;
+    cout <<"Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for (int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+    quickSort(arr,0,n-1);
+
+    cout << "Sorted array: ";
+    for (int i=0;i<n;i++) {
+        cout <<arr[i] << " ";
+    }
+
+    return 0;
+}
